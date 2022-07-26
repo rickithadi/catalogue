7571686385a6277b80f94b1d4dc881eb2578ae5b
Hadi Rickit <rickithadi@gmail.com>
2022-07-26 20:12:38 +0800

    add typings for png

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,16 @@
 import { StyleSheet } from 'react-native';
+import { Image} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import logo from '../assets/images/logo.png';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+<Image source={logo} style={{ width: 305, height: 159 }} />
+      <Text style={styles.title}>I like big butts</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>

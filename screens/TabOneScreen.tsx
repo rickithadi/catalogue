276bfd10973168efe572af8087a83ec72dc980e1
Hadi Rickit <rickithadi@gmail.com>
2022-07-27 09:23:47 +0800

    moved styles into Appstyles

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,31 +5,16 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import logo from '../assets/images/logo.png';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import AppStyles from '../styles/AppStyles';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-<Image source={logo} style={{ width: 305, height: 159 }} />
-      <Text style={styles.title}>I like big butts</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View style={AppStyles.container}>
+<Image source={logo} style={AppStyles.logo} />
+      <Text style={AppStyles.title}>I like big butts</Text>
+      <View style={AppStyles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});

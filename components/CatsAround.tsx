13a28d2862595cf9134df515fbd7e3994d218d52
Hadi Rickit <rickithadi@gmail.com>
2022-08-12 17:17:06 +0800

    cleaned up unused deps

--- components/CatsAround.tsx
+++ components/CatsAround.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import MapView, { Marker } from "react-native-maps";
-
-import { View, Text, ScrollView } from "react-native";
+import { View, Text } from "react-native";
+import { LocationGeocodedAddress } from "expo-location";
 
 import AppStyles from "../styles/AppStyles";
-
 import { Cat } from "../types";
-import { LocationGeocodedAddress, LocationObject } from "expo-location";
 
 export default function CatsAround(props: {
   cats: Cat[];
@@ -29,25 +25,6 @@ export default function CatsAround(props: {
           </Text>
         </TouchableOpacity>
       </View>
-      {/* <GooglePlacesAutocomplete
-        placeholder="Search"
-        textInputProps={{ onBlur: () => {} }}
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          console.log(data, details);
-        }}
-        query={{
-          key: "AIzaSyA2VHWlEdfIiMSU8nIMNMVAMChpU-H9s_M",
-          language: "en",
-        }}
-        currentLocation={true}
-        currentLocationLabel="Current location"
-        enablePoweredByContainer={false}
-        requestUrl={{
-          url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api",
-          useOnPlatform: "web",
-        }} // this in only required for use on the web. See https://git.io/JflFv more for details.
-      /> */}
       {props.cats.map((c: Cat) => (
         <View style={AppStyles.popCatContainer} key={c.uid}>
           <Text>{c.name}</Text>

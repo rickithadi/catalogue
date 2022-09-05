1aa5cddeb9ddf6d089892bbf1323afb4b30e9e2c
Hadi Rickit <rickithadi@gmail.com>
2022-09-05 15:37:37 +0800

    switched to supabase

--- App.tsx
+++ App.tsx
@@ -1,63 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useFonts } from "expo-font";
-import {
-  LondrinaSolid_100Thin,
-  LondrinaSolid_300Light,
-  LondrinaSolid_400Regular,
-  LondrinaSolid_900Black,
-} from "@expo-google-fonts/londrina-solid";
-import {
-  RobotoMono_100Thin,
-  RobotoMono_100Thin_Italic,
-  RobotoMono_300Light,
-  RobotoMono_300Light_Italic,
-  RobotoMono_400Regular,
-  RobotoMono_400Regular_Italic,
-  RobotoMono_500Medium,
-  RobotoMono_500Medium_Italic,
-  RobotoMono_700Bold,
-  RobotoMono_700Bold_Italic,
-} from "@expo-google-fonts/roboto-mono";
-import AppLoading from "expo-app-loading";
-
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
-import { setGoogleApiKey } from "expo-location";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  let [fontsLoaded] = useFonts({
-    LondrinaSolid_100Thin,
-    LondrinaSolid_300Light,
-    LondrinaSolid_400Regular,
-    LondrinaSolid_900Black,
-    RobotoMono_100Thin,
-    RobotoMono_100Thin_Italic,
-    RobotoMono_300Light,
-    RobotoMono_300Light_Italic,
-    RobotoMono_400Regular,
-    RobotoMono_400Regular_Italic,
-    RobotoMono_500Medium,
-    RobotoMono_500Medium_Italic,
-    RobotoMono_700Bold,
-    RobotoMono_700Bold_Italic,
-  });
-
-  // TODO use .env
-  setGoogleApiKey("AIzaSyA2VHWlEdfIiMSU8nIMNMVAMChpU-H9s_M")
-  if (!fontsLoaded || !isLoadingComplete) {
-    return <AppLoading />;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ttofpxcplkleqtuopfgq.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0b2ZweGNwbGtsZXF0dW9wZmdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzNjI1ODMsImV4cCI6MTk3NzkzODU4M30.8cmt3f65EN9cAxZUsf6syZLs_aSCbszkbC4A4kyQ-tA";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});

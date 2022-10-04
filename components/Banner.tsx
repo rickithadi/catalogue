620da27868310a1503860dc0ebddf707d26f564b
Hadi Rickit <rickithadi@gmail.com>
2022-10-04 14:54:23 +0800

    popular cat banner reading from whereabouts table

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, ImageBackground } from "react-native";
 import AppStyles from "../styles/AppStyles";
 
 import { Cat } from "../types/types";
 
 import catSample from "../assets/images/rusty.jpg";
+import { supabase } from "../lib/supabase";
 
 export default function Banner(props: { cat: Cat }) {
+  const [pictureList, setPictureList] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getCatPics = async () => {
+      // TODO fix array response
+      const { data: nestedPictureList, error } = await supabase
+        .from("whereabouts")
+        .select("pictures")
+        .eq("cat_id", props.cat.id);
+      if (nestedPictureList) {
+        console.log("pictureList", nestedPictureList);
+        const { pictures } = nestedPictureList[0];
+        setPictureList(pictures);
+      }
+      // setPictureList(pictureList);
+    };
+    getCatPics();
+  }, [props.cat]);
+
   return (
     <View style={AppStyles.bannerContainer}>
       <ImageBackground
-        source={catSample}
+        source={{ uri: pictureList[0] }}
         resizeMode="cover"
         style={AppStyles.image}
       >
